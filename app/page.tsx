d4616ecc09fb6c9d0a7bbabc60f30f7da4289ca6
Yash Gupta <gyash21@gmail.com>
2024-09-15 14:19:51 +0530

    update- complete hero section, image and intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Intro from "@/components/intro";
 
 export default function Home() {
   return (
     <div>
       <section className="py-24">
-        <div className="container">
+        <div className="container max-w-3xl">
+          <Intro />
         </div>
 
       </section>

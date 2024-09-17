2740ae8ba7bb870055322131f908cc5ebb0ea028
Yash Gupta <gyash21@gmail.com>
2024-09-17 18:22:39 +0530

    update- write first blog post

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Intro from "@/components/intro";
 import { MDXRemote } from "next-mdx-remote";
 
 export default function Home() {
-  
+    const content = `
+    # Hello, world!`
 
 
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <section className="py-24">
         <div className="container max-w-3xl">
           <Intro />
+          <MDXRemote source={content} />
         </div>
 
       </section>

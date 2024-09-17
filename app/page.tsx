e946167b8177b6d62c8fe15cec1f98146518684a
Yash Gupta <gyash21@gmail.com>
2024-09-17 18:16:14 +0530

    update- install and render MDX

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Intro from "@/components/intro";
+import { MDXRemote } from "next-mdx-remote";
 
 export default function Home() {
+  
+
+
   return (
     <div>
       <section className="py-24">
